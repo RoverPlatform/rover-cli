baaa5837b60e7f96757c212657b826f77861554b
Sean Rucker <sean@rover.io>
2017-08-29 14:15:17 -0400

    Implemented login functionality

--- rover-login.js
+++ rover-login.js
@@ -1,3 +1,181 @@
 #!/usr/bin/env node
 
-console.log("Coming soon...")
+const inquirer = require("inquirer")
+const request = require("request")
+
+function signIn() {
+    const self = this
+
+    let email
+    let password
+    let session
+    let account
+    let user
+
+    return Promise.resolve()
+        .then(inquire)
+        .then(createSession)
+        .then(fetchAccount)
+        .then(fetchUser)
+        .then(() => {
+            return self
+        })
+
+    function inquire() {
+        const questions = [{
+            name: "email",
+            message: "Email:"
+        }, {
+            type: "password",
+            name: "password",
+            message: "Password:"
+        }]
+
+        return inquirer.prompt(questions).then(answers => {
+            self.email = answers.email
+            self.password = answers.password
+        })
+    }
+
+    function createSession() {
+        return new Promise((resolve, reject) => {
+            const { email, password } = self
+            const body = JSON.stringify({
+                data: {
+                    attributes: {
+                        email,
+                        password
+                    },
+                    type: "sessions"
+                }
+            })
+
+            const options = {
+                body,
+                headers: {
+                    "accept": "application/vnd.api+json",
+                    "content-type": "application/vnd.api+json"
+                },
+                method: "POST",
+                url: "https://api.rover.io/v1/sessions"
+            }
+
+            request(options, function (error, response, body) {
+                if (error) {
+                    return reject(error)
+                }
+
+                if (response.statusCode != 200) {
+                    const error = new Error("Failed to create session")
+                    error.code = response.statusCode
+                    return reject(error)
+                }
+
+                const json = JSON.parse(body)
+                try {
+                    const data = json.data
+                    const relationships = data.relationships
+                    self.session = {
+                        id: data.id,
+                        token: data.attributes.token,
+                        userId: relationships.user.data.id,
+                        accountId: relationships.account.data.id
+                    }
+                    resolve()
+                } catch (error) {
+                    reject(error)
+                }
+            })
+        })
+    }
+
+    function fetchAccount() {
+        return new Promise((resolve, reject) => {
+            const { token, accountId } = self.session
+            const options = {
+                headers: {
+                    "accept": "application/vnd.api+json",
+                    "authorization": "Bearer " + token
+                },
+                method: "GET",
+                url: "https://api.rover.io/v1/accounts/" + accountId
+            }
+
+            request(options, function (error, response, body) {
+                if (error) {
+                    return reject(error)
+                }
+
+                if (response.statusCode != 200) {
+                    const error = new Error("Failed to fetch account")
+                    error.code = response.statusCode
+                    return reject(error)
+                }
+
+                const json = JSON.parse(body)
+                try {
+                    const data = json.data
+                    self.account = {
+                        id: data.id,
+                        title: data.attributes.title
+                    }
+                    resolve()
+                } catch (error) {
+                    reject(error)
+                }
+            })
+        })
+    }
+
+    function fetchUser() {
+        return new Promise((resolve, reject) => {
+            const { token, userId } = self.session
+            const options = {
+                headers: {
+                    "accept": "application/vnd.api+json",
+                    "authorization": "Bearer " + token
+                },
+                method: "GET",
+                url: "https://api.rover.io/v1/users/" + userId
+            }
+
+            request(options, function (error, response, body) {
+                if (error) {
+                    return reject(error)
+                }
+
+                if (response.statusCode != 200) {
+                    const error = new Error("Failed to fetch user")
+                    error.code = response.statusCode
+                    return reject(error)
+                }
+
+                const json = JSON.parse(body)
+                try {
+                    const data = json.data
+                    self.user = {
+                        id: data.id,
+                        name: data.attributes.name
+                    }
+                    resolve()
+                } catch (error) {
+                    reject(error)
+                }
+            })
+        })
+    }
+}
+
+(function login() {
+
+    const success = ({ session, account, user }) => {
+        console.log("Successfully signed in as " + user.name + " (" + account.title + ")")
+    }
+
+    const retry = (err) => {
+        console.log("Invalid email or password")
+        login()
+    }
+
+    signIn().then(success).catch(retry)
+})()
